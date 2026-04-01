5fc36cd0c32d6c9851c8f3bf2ddabe9f390489f1
amlaxmana <amlaxmana.student@ua.edu.ph>
2026-04-01 14:51:08 +0800

    Sawakas

--- pages/auth/RegisterPage.jsx
+++ pages/auth/RegisterPage.jsx
@@ -6,7 +6,7 @@ import CertiLogo from '../../src/Images/CertiLogo.png';
 
 
 const SCHOOL_EMAIL_DOMAIN = '@ua.edu.ph';
-const API_BASE = 'https://certifierfrontend.onrender.com';
+const API_BASE = 'https://certifierbackend.onrender.com';
 
 const RegisterPage = () => {
   const navigate = useNavigate();
